a51bc064ba156f7808e6fb5bc14e8864e673a9a6
Divyanshu Sahu <257divyanshu@gmail.com>
2025-05-22 11:47:17 +0530

    add lecture 1 notes

---
+++ extra_notes/notes.js
@@ -0,0 +1,4 @@
+// 📍 on running git push, we got this error : 
+// error: src refspec refs/heads/main does not match any
+// error: failed to push some refs to 'https://github.com/257divyanshu/javascript_data_structures.git'
+// - means that Git couldn't find a branch named main to push
